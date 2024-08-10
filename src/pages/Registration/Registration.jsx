ed7bbd676bf9698c3dde3a523103a660988e2e56
Jay <akinjidesewanu@gmail.com>
2024-08-10 04:00:02 +0100

    initail commit

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -7,13 +7,13 @@ import dlw from '../../assets/registrationpage/dlw.jpeg';
 export default function Registration() {
     return (
         
-        <div className="flex items-center justify-center gap-20 lg:w-12/6 h-screen text-blue-900 ">
+        <div className="flex flex-col lg:p-10 p-5 items-center justify-center gap-20 lg:flex-row  text-blue-900 lg:h-screen bg-[#F5F5F5]">
                     
-                <div>
+                <div className="bg-[#fff] rounded-lg flex flex-col gap-2 px-9 py-4'">
                     <img className="w-[300px] mb-1" src={Logo} alt="Logo" />
                     <h1 className="text-4xl font-bold mb-1">
                     2024 <span className="text-red-600">Camp</span> Registration
-                </h1>
+                    </h1>
                     <div className="text-[15px]">
                        <label>First Name</label>
                         <input 
@@ -30,57 +30,67 @@ export default function Registration() {
                         /> 
                     </div>
 
-                    <div className="flex gap-10 text-[15px]">
+                    <div className="flex flex-col  lg:gap-2 text-[15px]">
                         <div>
                             <label>Phone Number</label>
                             <input 
-                            className="w-full border border-blue-900 rounded-md p-2 mt-1 "
+                            className="w-full border border-blue-900 rounded-md p-2 mt-1"
                             placeholder="Enter Phone Number" required
                             /> 
                         </div>
 
                         <div>
                             <label>Gender</label>
-
                             <select className="w-full border border-blue-900 rounded-md p-2 mt-1"  
                                 name="gender" id="gender" required>
                                 <option value="">-Select Gender-</option>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
-                                <option value="Other">Other</option> 
                             </select>
                         </div>
                     </div>
-                    <div className="text-[15px]">
-                        <label>Upload Profile Image</label>
-                        <input 
-                        className="w-full border border-blue-900 rounded-md p-2 mt-1 "
-                        placeholder="Select File To Be Uploaded" required
-                        /> 
-                    </div>
+                    
 
-                    <div className="flex gap-2 text-[15px]">
+                    <div className="flex flex-col lg:flex-row lg:gap-2 text-[15px]">
                         <div>
                             <label>Archdeaconry</label>
                             <select className="w-full border border-blue-900 rounded-md p-2 mt-1"
                                 name="Archdeaconry" id="Archdeaconry" required>
                                 <option value="">-Select Your Archdeaconry-</option>
-                                <option value="Lagos">Lagos</option>
-                                <option value="Lagos">Lagos</option>
-                                <option value="Lagos">Other</option>
+                                <option value="Abule Egba">Abule Egba</option>
+                                <option value="Agege">Agege</option>
+                                <option value="Amuwo Odofin">Amuwo Odofin</option>
+                                <option value="Festac">Festac</option>
+                                <option value="Gowon Estate">Gowon Estate</option>
+                                <option value="Iba">Iba</option>
+                                <option value="Idimu">Idimu</option>
+                                <option value="Ijede">Ijede</option>
+                                <option value="Ikeja">Ikeja</option>
+                                <option value="Ikorodu-North">Ikorodu-North</option>
+                                <option value="Ikosi-Ketu">Ikosi-Ketu</option>
+                                <option value="Imota">Imota</option>
+                                <option value="Ipaja">Ipaja</option>
+                                <option value="Isolo">Isolo</option>
+                                <option value="Ogudu">Ogudu</option>
+                                <option value="Ojo">Ojo</option>
+                                <option value="Ojo-Alaba">Ojo-Alaba</option>
+                                <option value="Ojo-Alaba">Ojo-Alaba</option>
+                                <option value="Opebi">Opebi</option>
+                                <option value="Opebi">Opebi</option>
+                                <option value="Oto-Awori'">Oto-Awori</option>
+                                <option value="Owutu">Owutu</option>
+                                <option value="Satallite">Satallite</option>
+                                <option value="Somolu">Somolu</option>
+                                <option value="Ikorodu">Ikorodu</option>
                             </select>
                         </div>
 
                         <div className="text-[15px]">
                             <label>Parish</label>
-
-                            <select className="w-full border border-blue-900 rounded-md p-2 mt-1"
-                                name="gender" id="gender" required>
-                                <option value="">-Select Your Parish-</option>
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                                <option value="Other">Other</option>
-                            </select>
+                            <input 
+                            className="w-full border border-blue-900 rounded-md p-2 mt-1"
+                            placeholder="Enter Parish" required
+                            /> 
                         </div>
                     </div>
                     <div className="text-[15px]">
@@ -100,8 +110,8 @@ export default function Registration() {
                 
            
            
-           <div className="hiddden lg:flex h-full items-center justify-center ">
-                <div className="gap-4">
+           <div className="lg:flex flex-col hidden items-center justify-center ">
+                <div className="lg:flex-col items-center justify-center">
                     <img className="w-[600px] border " src={dlw} alt="" />
 
                     <div className="text-center mt-1 mb-1">
@@ -111,9 +121,9 @@ export default function Registration() {
                         <p className="text-blue-900 font-semibold ">
                         “ But now is made manifest, and by the scriptures of the <br /> prophets, according to the commandment of the everlasting <br />God, made known to all nations for the obedience of faith: “
                         </p>
-                     </div>
+                        
+                    </div>
                 </div>
-                
            </div>
            
         </div>
