702868c1c93b69c9297101af4dc808dff03c14d3
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-08-27 16:34:13 +0100

    Merge Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 const Header = (e) => (
-     <div className="banner lg:h-[25vh] h-[17vh] bg-[#091e5465] flex">
-          <div className="h-[17vh] bg-[#00000005] flex items-end p-5 w-full">
+     <div className="banner lg:h-[30vh] h-[17vh] bg-[#091e5465] flex">
+          <div className="lg:h-[30vh] h-[17vh] bg-[#00000005] flex items-end p-5 w-full">
                 <h1 className="font-header text-white text-[25px] lg:text-[35px]">{e.text}</h1>
           </div>
      </div>
